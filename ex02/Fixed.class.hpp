0c869c1eeaaf9ea2cd53c3afc8504b054d4abad8
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-26 04:23:07 +0100

    prototyping overload of min max

--- ex02/Fixed.class.hpp
+++ ex02/Fixed.class.hpp
@@ -14,29 +14,34 @@ class Fixed
 
 	public:
 
-		int		getRawBits(void) const;
-		void	setRawBits(int const raw);
-		float	toFloat(void) const;
-		int		toInt(void) const;
+		int					getRawBits(void) const;
+		void				setRawBits(int const raw);
+		float				toFloat(void) const;
+		int					toInt(void) const;
+
+		static Fixed		&min(Fixed &value1, Fixed &value2);
+		static Fixed		&max(Fixed &value1, Fixed &value2);
+		static const Fixed	&min(Fixed const &value1, Fixed const &value2);
+		static const Fixed	&max(Fixed const &value1, Fixed const &value2);
 		
-		bool	operator==(Fixed const &param) const;
-		bool	operator!=(Fixed const &param) const;
-		bool	operator<=(Fixed const &param) const;
-		bool	operator>=(Fixed const &param) const;
-		bool	operator<(Fixed const &param) const;
-		bool	operator>(Fixed const &param) const;
+		bool				operator==(Fixed const &param) const;
+		bool				operator!=(Fixed const &param) const;
+		bool				operator<=(Fixed const &param) const;
+		bool				operator>=(Fixed const &param) const;
+		bool				operator<(Fixed const &param) const;
+		bool				operator>(Fixed const &param) const;
 
-		Fixed	operator+(Fixed const &param) const;
-		Fixed	operator-(Fixed const &param) const;
-		Fixed	operator*(Fixed const &param) const;
-		Fixed	operator/(Fixed const &param) const;
+		Fixed				operator+(Fixed const &param) const;
+		Fixed				operator-(Fixed const &param) const;
+		Fixed				operator*(Fixed const &param) const;
+		Fixed				operator/(Fixed const &param) const;
 
-		Fixed	&operator++(void);
-		Fixed	&operator--(void);
-		Fixed	operator++(int);
-		Fixed	operator--(int);
+		Fixed				&operator++(void);
+		Fixed				&operator--(void);
+		Fixed				operator++(int);
+		Fixed				operator--(int);
 
-		Fixed 	&operator=(Fixed const &assign);
+		Fixed 				&operator=(Fixed const &assign);
 
 		Fixed(void);
 		Fixed(const int param);
@@ -46,6 +51,6 @@ class Fixed
 
 };
 
-std::ostream &	operator<< (std::ostream &o, Fixed const &i);
+std::ostream 	&operator<< (std::ostream &o, Fixed const &i);
 
 #endif
