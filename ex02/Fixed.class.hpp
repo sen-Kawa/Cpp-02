5fb81118a77288bc151989f1aacc8a336234c442
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-26 04:12:53 +0100

    overloading operators

---
+++ ex02/Fixed.class.hpp
@@ -0,0 +1,51 @@
+#ifndef FIXED_CLASS_HPP
+# define FIXED_CLASS_HPP
+
+#include <iostream>
+#include <ostream>
+#include <cmath>
+
+class Fixed
+{
+	private:
+
+		int					number;
+		static const int	fractional_bits = 8;
+
+	public:
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+		
+		bool	operator==(Fixed const &param) const;
+		bool	operator!=(Fixed const &param) const;
+		bool	operator<=(Fixed const &param) const;
+		bool	operator>=(Fixed const &param) const;
+		bool	operator<(Fixed const &param) const;
+		bool	operator>(Fixed const &param) const;
+
+		Fixed	operator+(Fixed const &param) const;
+		Fixed	operator-(Fixed const &param) const;
+		Fixed	operator*(Fixed const &param) const;
+		Fixed	operator/(Fixed const &param) const;
+
+		Fixed	&operator++(void);
+		Fixed	&operator--(void);
+		Fixed	operator++(int);
+		Fixed	operator--(int);
+
+		Fixed 	&operator=(Fixed const &assign);
+
+		Fixed(void);
+		Fixed(const int param);
+		Fixed(const float param);
+		Fixed(Fixed const &src);
+		~Fixed(void);
+
+};
+
+std::ostream &	operator<< (std::ostream &o, Fixed const &i);
+
+#endif
