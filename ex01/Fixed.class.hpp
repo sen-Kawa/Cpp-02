228f15e03ee3ecad1591be81a6d2245046dc4dfd
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-26 02:59:12 +0100

    to int and to float member functions added

--- ex01/Fixed.class.hpp
+++ ex01/Fixed.class.hpp
@@ -14,6 +14,8 @@ class Fixed
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 		
 		Fixed &	operator=(Fixed const &assign);
 
